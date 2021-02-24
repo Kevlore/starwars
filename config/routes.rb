b0f4122dc5672db55f2eb99760b85feaf9069a7e
Logan <logalamo@gmail.com>
2021-02-23 22:31:00 -0600

    Created controllers for films, characters, and species

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'species/index'
+  get 'species/show'
+  get 'characters/index'
+  get 'characters/show'
+  get 'films/index'
+  get 'films/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
