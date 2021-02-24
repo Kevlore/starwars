6ac56f7e66c332c50e4d252511421471a9279608
Logan <logalamo@gmail.com>
2021-02-23 22:36:29 -0600

    Added Routes for all the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'species/index'
-  get 'species/show'
-  get 'characters/index'
-  get 'characters/show'
-  get 'films/index'
-  get 'films/show'
+  # get 'species/index'
+  # get 'species/show'
+  # get 'characters/index'
+  # get 'characters/show'
+  # get 'films/index'
+  # get 'films/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :films, only: [:index, :show]
+  resources :characters, only: [:index, :show]
+  resources :species, only: [:index, :show]
 end
